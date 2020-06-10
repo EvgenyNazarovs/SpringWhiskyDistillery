e55407edffd4cb60e06f7e5af70dff4a8c512c79
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-06-10 22:30:20 +0100

    adds whisky and distillery controllers

---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,49 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canGetWhiskyForParticularYear() {
+		List<Whisky> found = whiskyRepository.findByYear(1991);
+		assertEquals(1, found.size());
+	}
+
+	@Test
+	public void canGetDistilleriesByRegion() {
+		List<Distillery> found = distilleryRepository.findByRegion("Highland");
+		assertEquals(3, found.size());
+	}
+
+	@Test
+	public void canGetWhiskyByDistilleryAndAge() {
+		List<Whisky> found = whiskyRepository.findByDistilleryNameAndAge("Highland Park", 12);
+		assertEquals(1, found.size());
+	}
+
+}
